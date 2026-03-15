fe20de68f893d229453f560ec9abff1b817ae0f5
rk868726-alt <rk868726@gmail.com>
2026-03-15 16:16:32 +0530

    Update mirror.js

--- commands/mirror.js
+++ commands/mirror.js
@@ -5,17 +5,21 @@ module.exports = {
 
  execute(message, args) {
 
+  if (!message.member.permissions.has("ManageChannels"))
+   return message.reply("You need Manage Channels permission")
+
   const from = message.mentions.channels.first()
   const to = message.mentions.channels.last()
 
-  if (!from || !to) return message.reply("Usage: !mirror #from #to")
+  if (!from || !to)
+   return message.reply("Usage: !mirror #from-channel #to-channel")
 
-  const data = JSON.parse(fs.readFileSync("./data/mirror.json"))
+  let data = JSON.parse(fs.readFileSync("./data/mirror.json"))
 
   data[from.id] = to.id
 
-  fs.writeFileSync("./data/mirror.json", JSON.stringify(data))
+  fs.writeFileSync("./data/mirror.json", JSON.stringify(data, null, 2))
 
-  message.channel.send("Mirror set")
+  message.reply(`Messages from ${from} will now mirror to ${to}`)
  }
 }
